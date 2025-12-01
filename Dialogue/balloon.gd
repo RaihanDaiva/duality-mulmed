d8768a650e87a99c8d3d8dd6675026b47eef208e
ThriF3 <firmanfawnia.f12@gmail.com>
2025-12-01 17:06:24 +0700

    UI: Adding Portrait on Dialogue

--- Dialogue/balloon.gd
+++ Dialogue/balloon.gd
@@ -55,6 +55,9 @@ var mutation_cooldown: Timer = Timer.new()
 ## The base balloon anchor
 @onready var balloon: Control = %Balloon
 
+## Portrait
+@onready var portrait: TextureRect = %Portrait
+
 ## The label showing the name of the currently speaking character
 @onready var character_label: RichTextLabel = %CharacterLabel
 
@@ -128,6 +131,11 @@ func apply_dialogue_line() -> void:
 
 	character_label.visible = not dialogue_line.character.is_empty()
 	character_label.text = tr(dialogue_line.character, "dialogue")
+	var portrait_path: String = "res://Dialogue/assets/%s-portrait.png" % dialogue_line.character
+	if ResourceLoader.exists(portrait_path):
+		portrait.texture = load(portrait_path)
+	else:
+		portrait.texture = null
 
 	dialogue_label.hide()
 	dialogue_label.dialogue_line = dialogue_line
